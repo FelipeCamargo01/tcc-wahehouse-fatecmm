652e7f1ee14ab2662be7a11104b0a66e1f2b59b4
Felipe Camargo <36897453+FelipeCamargo01@users.noreply.github.com>
2022-11-13 17:17:11 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,8 @@ connection
 console.log("1");
 require('dotenv').config();
 console.log("2");
-const PORT = process.env.SERVER_PORT:8080
+const PORT = process.env.SERVER_PORT || 8080
+console.log(PORT);
 console.log("3");
 const app = express();
 console.log("4");
