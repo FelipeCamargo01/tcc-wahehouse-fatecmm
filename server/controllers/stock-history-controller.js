32bd51d914895a005e206d59d1bab102b2a666f5
Luiz Felipe <llf3628@gmail.com>
2022-11-17 21:03:24 -0300

    Alterando criação de stock movimentation

--- server/controllers/stock-history-controller.js
+++ server/controllers/stock-history-controller.js
@@ -9,11 +9,21 @@ class ProductController {
         const product = await Product.findOne({raw: true},{ where: { SKU: body.SKU } });
         if(!product) throw new Error('Produto não encontrado');
 
+        movimentationType = '';
+        movimentationDate = body.actionDate ? body.actionDate : moment().format('YYYY-MM-DD HH:mm:ss');
+
+        if(body.type === 0) {
+            movimentationType = 'Saída';
+        }
+        else if(body.type === 1) {
+            movimentationType = 'Entrada';
+        }
+
         await StockHistory.create({
-            type: body.type,
+            type: movimentationType,
             productId: product.SKU,
             quantity: body.quantity,
-            actionDate: moment(body.actionDate).format('YYYY-MM-DD HH:mm:ss')
+            actionDate: movimentationDate
         });
 
         return ResponseParse.response("OK", 'Movimentação criada com sucesso!');
