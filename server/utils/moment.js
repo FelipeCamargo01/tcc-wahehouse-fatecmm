9b493015bbf9c61324819fa90f90bb3a0a2fa029
Luiz <llf3628@gmail.com>
2022-12-11 19:08:56 -0300

    Ajuste busca dados gráfico

--- server/utils/moment.js
+++ server/utils/moment.js
@@ -2,8 +2,8 @@ const moment = require ('moment');
 
 function getMondayDayOfWeek() {
     var day = moment().startOf('week').format('dddd') === 'Sunday' ?     
-    moment().startOf('week').format('dddd DD-MM-YYYY') : 
-    moment().startOf('week').format('dddd DD-MM-YYYY');
+    moment().startOf('week') : 
+    moment().startOf('week').remove('d',1);
 
     console.log('getMondayDayOfWeek', day);
 
@@ -12,8 +12,8 @@ function getMondayDayOfWeek() {
 
 function getSundayDayOfWeek() {
     var day = moment().startOf('week').format('dddd') === 'Sunday' ?     
-    moment().startOf('week').add('d',6).format('dddd DD-MM-YYYY') : 
-    moment().startOf('week').format('dddd DD-MM-YYYY');
+    moment().startOf('week').add('d',6) : 
+    moment().startOf('week');
 
     console.log('getSundayDayOfWeek', day);
 
